479cc8868e10a0069356f65babc625ad5684771f
saulm96 <saulmorahernandez96@gmail.com>
2025-01-22 09:08:42 +0100

    Pre change

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use(express.static("src/public"));
 
 app.use('/', router);
 
-app.listen(process.env.PORT, () => {
+app.listen(3000, () => {
     console.log(`Server started on port ${process.env.APP_PORT}`);
 });
 
