c42df34d1e54f1bf3517892174e890486f34c15e
saulm96 <saulmorahernandez96@gmail.com>
2025-01-26 18:26:51 +0100

    JWT authentication  included

--- src/utils/errors/userErrors.js
+++ src/utils/errors/userErrors.js
@@ -1,24 +1,18 @@
-class UserError extends Error {
-    constructor(message, statusCode = 500) {
-        super(message);
-        this.name = 'UserError';
-        this.statusCode = statusCode;
+class USER_NOT_FOUND extends Error{
+    constructor(){
+        super("User not found");
+        this.status = 404;
     }
 }
 
-class USER_NOT_FOUND extends UserError {
-    constructor() {
-        super('User not found', 404);
-        this.name = 'UserNotFoundError';
-    }
-} 
-
-class INVALID_PASSWORD extends UserError {
-    constructor() {
-        super('Invalid password', 401);
-        this.name = 'InvalidPasswordError';
+class INVALID_PASSWORD extends Error{
+    constructor(){
+        super("Invalid password");
+        this.status = 401;
     }
 }
+
+
 
 export default {
     USER_NOT_FOUND,
