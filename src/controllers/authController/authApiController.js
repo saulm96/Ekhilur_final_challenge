f8395a084756745e0e1183ab0a02616b8bf4e9ef
saulm96 <saulmorahernandez96@gmail.com>
2025-01-29 13:10:01 +0100

    Fixed a bug related with the secret regen

--- src/controllers/authController/authApiController.js
+++ src/controllers/authController/authApiController.js
@@ -35,7 +35,7 @@ async function login(req, res) {
 
         return res.json({
             success: true,
-            secret: user.secret,
+            secret: checkingUser.two_factor_secret,
             message: "Please enter this secret in Google Authenticator and verify the token to complete login!"
         });
 
